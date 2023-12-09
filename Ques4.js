3d67c6ccc0f4c85de8cd3323bc993c05ec4441b9
Nikhil Seth <nseth435@gmail.com>
2023-12-09 10:29:57 +0530

    Array

---
+++ Ques4.js
@@ -0,0 +1,23 @@
+/*Write a JavaScript program to sort the items of an array.
+Sample array : var arr1 = [ -3, 8, 7, 6, 5, -4, 3, 2, 1 ];
+Sample Output : -4,-3,1,2,3,5,6,7,8*/
+
+const arr=[-3,8,7,6,5,-4,3,2,1];
+// arr.toString();
+l=arr.length;
+l=l-1;
+let temp=0;
+for(let i=1;i<l;i++)
+{
+   for(let j=0; j<l; j++)
+    {
+        if(arr[j]>arr[j+1])
+        {
+            temp=arr[j+1];
+            arr[j+1]=arr[j];
+            arr[j]=temp;
+        }
+    }
+
+}
+console.log(arr.join())
